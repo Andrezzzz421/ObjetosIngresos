7fd279ce67379799a25e6261b4338d14b2b129c9
Andrezzzz421 <andresdavidproxd@gmail.com>
2026-05-11 07:07:38 -0500

    Login

--- ObjetosIngresos/Controllers/UsuariosController.cs
+++ ObjetosIngresos/Controllers/UsuariosController.cs
@@ -1,8 +1,97 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using FirebaseAdmin.Auth;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ObjetosIngresos.Models;
+using ObjetosIngresos.Services;
+using System.Security.Claims;
 
 namespace ObjetosIngresos.Controllers
 {
-    public class UsuariosController:Controller
+    public class UsuariosController : Controller
     {
+        private readonly SistemaIngresoContext db;
+        private readonly UsuarioServices ser;
+
+        public UsuariosController(SistemaIngresoContext db)
+        {
+            this.db = db;
+            ser = new UsuarioServices(db);
+        }
+
+        private void CargarCombos(Usuario u = null)
+        {
+            ViewBag.Sedes = new SelectList(db.Sedes, "IdSede", "Nombre", u?.IdSedePrincipal);
+            ViewBag.TipoUsuarios = new SelectList(db.TiposUsuarios, "IdTipoUsuario", "Descripcion", u?.IdTipoUsuario);
+        }
+        public ActionResult Index()
+        {
+            return View(ser.GetAll());
+        }
+
+        public ActionResult Create()
+        {
+            CargarCombos();
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Usuario us)
+        {
+            if (ModelState.IsValid)
+            {
+                ser.Add(us);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(us);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var user = ser.GetById(id);
+
+            if (user == null)
+            {
+                NotFound();
+            }
+            CargarCombos();
+            return View(user);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Usuario us)
+        {
+            try
+            {
+
+                if (ModelState.IsValid)
+                {
+                    ser.Update(us);
+                    return RedirectToAction(nameof(Index));
+                }
+                CargarCombos();
+                return View(ser.GetAll());
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult Delete(int id)
+        {
+            bool eliminado = ser.Delete(id);
+
+            if (!eliminado)
+            {
+                TempData["Error"] = "No se puede eliminar el usuario porque tiene registros vinculados.";
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
+
     }
 }
+      
